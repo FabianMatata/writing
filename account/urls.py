a5aa67cce6a394eec739dd96e99412e4813ae951
FabianMatata <mfabian601@gmail.com>
2022-10-05 18:21:23 +0300

    models

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name= 'index'),
     path('login/', views.login_view, name='login'),
-    path('register', views.register, name='register'),
+    path('register/', views.register, name='register'),
+    path('adminpage/', views.admin, name='adminpage'),
+    path('customer/', views.customer, name='customer'),
+    path('employee/', views.employee, name='employee'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-#def index(request): 
-    #return render(request, 'index.html')
+def index(request): 
+    return render(request, 'index.html')
 
 def register(request):
     msg = None
@@ -14,14 +14,14 @@ def register(request):
         if form.is_valid(): 
             user = form.save()
             msg = 'user created'
-            return redirect('login_view') 
+            return redirect('login') 
         else: 
             msg = 'form is not valid'
     else: 
         form = SignUpForm()
     return render(request, 'register.html', {'form': form, 'msg': msg})
 
-def logon_view(request): 
+def login_view(request): 
     form = LoginForm(request.POST or None)
     msg = None
     if request.method == 'POST': 
@@ -29,9 +29,26 @@ def logon_view(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            if user is not None: 
+            if user is not None and user.is_admin: 
                 login(request, user)
-                return redirect('home')
+                return redirect('adminpage')
+            elif user is not None and user.is_customer:
+                login(request, user)
+                return redirect('customer')
+            elif user is not None and user.is_employee:
+                login(request, user)
+                return redirect('employee')   
             else: 
                 msg='invalid credentials'
-        else
+        else:
+            msg = 'error validating form'
+    return render(request, 'login.html', {'form': form, 'msg': msg})
+
+def admin(request):
+    return render(request, 'admin.html')
+
+def customer(request):
+    return render(request, 'customer.html')
+
+def employee(request):
+    return render(request, 'employee.html')
